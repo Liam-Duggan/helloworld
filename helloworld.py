920562683f60475589c9605bc4e245d20a09ec40
Liam-Duggan <160242105+Liam-Duggan@users.noreply.github.com>
2024-03-19 13:24:39 +0000

    Added missing input to country variable. Created a string with the data and saved the string to a text file

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,8 @@
 name = input("What is your name?: ")
 age = input("How old are you?: ")
-country = ("What country do you live in?: ")
+country = input("What country do you live in?: ")
+
+data_string = name + " is " + str(age) + " years old. They live in " + country + "."
+
+with open("users.txt","w+") as file:
+  file.write(data_string)
